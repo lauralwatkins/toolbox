31b1d57441b0a8e380bec49f7a92f7dcfa9a20ee
Laura L Watkins <lauralwatkins@gmail.com>
2014-01-28 16:19:55 -0500

    TOOLBOX: add function that writes time in seconds in a human readble format.

---
+++ fmttime.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+# FMTTIME
+# Laura L Watkins [lauralwatkins@gmail.com]
+# -----------------------------------------------------------------------------
+
+from numpy import abs, log, pi, sqrt
+
+def fmttime( time ):
+    
+    """
+    Output time elapsed in seconds into a sensible format.
+    
+    INPUTS
+      time : input time elapsed [seconds]
+    """
+    
+    
+    sc = time % 60.
+    time = ( time - sc ) / 60.
+    result = "{:}s".format( sc )
+    if not time: return result
+    
+    mn = int( time % 60. )
+    time = ( time - mn ) / 60.
+    result = "{:}m {:}".format( mn, result )
+    if not time: return result
+    
+    hr = int( time % 24. )
+    time = ( time - hr ) / 24.
+    result = "{:}h {:}".format( hr, result )
+    if not time: return result
+    
+    dy = int( time % 7. )
+    time = ( time - dy ) / 7.
+    result = "{:}d {:}".format( dy, result )
+    
+    return result
