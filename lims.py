e48821e8a15e51509142ce1e3744ca47d0e71131
Laura L Watkins <lauralwatkins@gmail.com>
2014-05-24 22:44:19 -0400

    TOOLBOX: lims can take different plus/minus errors.

--- lims.py
+++ lims.py
@@ -4,7 +4,7 @@
 # Laura L Watkins [lauralwatkins@gmail.com]
 # -----------------------------------------------------------------------------
 
-from numpy import array, log10, size
+from numpy import array, log10, shape, size
 
 
 def lims( x, f=0., log=False, err=0, pivot=None ):
@@ -22,24 +22,35 @@ def lims( x, f=0., log=False, err=0, pivot=None ):
       f     : extra padding factor [default:0.] (can have different upper and
               lower padding by providing f as a list or tuple)
       log   : pad limits on log scale
-      err   : errors on x, so limits will include error bars
+      err   : errors on x, so limits will include error bars (can have
+              different upper/lower errors by passing as (plus,minus) tuple)
       pivot : make limits symmetric around pivot point (use largest offset)
     """
     
-    xmin = ( x - err ).min()
-    xmax = ( x + err ).max()
+    
+    # check if plus/minus errors are different
+    if shape(err) == (2,) + shape(x):
+        perr = err[0]
+        merr = err[1]
+    else:
+        perr = err
+        merr = err
+    
+    xmin = ( x - merr ).min()
+    xmax = ( x + perr ).max()
     
     if log:
         xmin = log10( xmin )
         xmax = log10( xmax )
     
+    if pivot != None:
+        offset = max( pivot - xmin, xmax - pivot )
+        xmin = pivot - offset
+        xmax = pivot + offset
+    
     xptp = xmax - xmin
     if size(f) == 1: f = [f]
     lims = array( [ xmin - xptp * f[0], xmax + xptp * f[-1] ] )
-    
-    if pivot != None:
-        offset = abs( lims - pivot ).max()
-        lims = pivot + array( [ -1., 1. ] ) * offset
     
     if log: lims = 10**lims
     
