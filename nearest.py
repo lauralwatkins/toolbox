65294d9f1d49acaf4499a2b5e697f22df1cd2a96
Laura L Watkins <lauralwatkins@gmail.com>
2014-08-26 11:48:21 -0400

    STATS/TOOLBOX: move round-to-nearest function from stats to toolbox.

---
+++ nearest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+# STATS.NEAREST
+# Laura L Watkins [lauralwatkins@gmail.com]
+# -----------------------------------------------------------------------------
+
+
+
+def nearest( x, base=1. ):
+    
+    """
+    Round the inputs to the nearest base.  Beware, due to the nature of
+    floating point arithmetic, this maybe not work as you expect.
+    
+    INPUTS
+      x : input value of array
+    
+    OPTIONS
+      base : number to which x should be rounded
+    """
+    
+    return round( x / base ) * base
