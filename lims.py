bf52189e7fc1c1ce17332fb656b733d6e67e2488
Laura L Watkins <lauralwatkins@gmail.com>
2014-09-01 18:49:03 -0400

    TOOLBOX: lims ignores nan values.

--- lims.py
+++ lims.py
@@ -4,7 +4,7 @@
 # Laura L Watkins [lauralwatkins@gmail.com]
 # -----------------------------------------------------------------------------
 
-from numpy import array, log10, shape, size
+from numpy import *
 
 
 def lims( x, f=0., log=False, err=0, pivot=None ):
@@ -13,7 +13,7 @@ def lims( x, f=0., log=False, err=0, pivot=None ):
     Return the minimum and maximum of a distribution.  There is an option to
     pad the limits by an additional factor f, this is especially useful when
     calculating limits for a plot.  If f is given with two elements, then the
-    upper and lower padding will be different.
+    upper and lower padding will be different.  The program ignores nan values.
     
     INPUTS
       x : data for which limits required
@@ -36,21 +36,21 @@ def lims( x, f=0., log=False, err=0, pivot=None ):
         perr = err
         merr = err
     
-    xmin = ( x - merr ).min()
-    xmax = ( x + perr ).max()
+    xmin = nanmin(x-merr)
+    xmax = nanmax(x+perr)
     
     if log:
-        xmin = log10( xmin )
-        xmax = log10( xmax )
+        xmin = log10(xmin)
+        xmax = log10(xmax)
     
     if pivot != None:
-        offset = max( pivot - xmin, xmax - pivot )
-        xmin = pivot - offset
-        xmax = pivot + offset
+        offset = max(pivot-xmin, xmax-pivot)
+        xmin = pivot-offset
+        xmax = pivot+offset
     
     xptp = xmax - xmin
     if size(f) == 1: f = [f]
-    lims = array( [ xmin - xptp * f[0], xmax + xptp * f[-1] ] )
+    lims = array([ xmin - xptp * f[0], xmax + xptp * f[-1] ])
     
     if log: lims = 10**lims
     
