a366ec9f71821fea7d10bfd090a70c097c5b46a8
Laura L Watkins <lauralwatkins@gmail.com>
2015-06-26 16:09:49 -0400

    Housekeeping for fit_gauss and fmttime.

--- fit_gauss.py
+++ fit_gauss.py
@@ -4,8 +4,8 @@
 # Laura L Watkins [lauralwatkins@gmail.com]
 # -----------------------------------------------------------------------------
 
-from numpy import *
-from matplotlib.pyplot import *
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy import optimize, stats
 
 
@@ -24,47 +24,47 @@ def fit_gauss(data, bins=100, showplot=False, label=None):
     """
     
     # normalised histogram of data
-    yh, lims = histogram(data, bins=bins, normed=True)
+    yh, lims = np.histogram(data, bins=bins, normed=True)
     xh = (lims[1:] + lims[:-1])/2.
     
     # fit gaussian to distribution
-    p0 = array([ data.mean(), data.std() ])
+    p0 = np.array([ data.mean(), data.std() ])
     p, cov = optimize.curve_fit(stats.norm.pdf, xh, yh, p0)
     p[1:] = abs(p[1:])
     
     if showplot:
         
         # set up plotting
-        rc('font', family='serif')
-        rc('text', usetex=True)
-        rc('xtick', labelsize='8')
-        rc('ytick', labelsize='8')
-        rc('axes', labelsize='10')
-        rc('legend', fontsize='9')
+        plt.rc('font', family='serif')
+        plt.rc('text', usetex=True)
+        plt.rc('xtick', labelsize='8')
+        plt.rc('ytick', labelsize='8')
+        plt.rc('axes', labelsize='10')
+        plt.rc('legend', fontsize='9')
         
         # set up figure
-        fig = figure(figsize=(4,3))
+        fig = plt.figure(figsize=(4,3))
         fig.subplots_adjust(left=0.15, bottom=0.13, top=0.97, right=0.97)
         
         # plot histogram
-        hist(data, bins=bins, histtype="stepfilled", alpha=0.3, color="grey",
-            normed=True)
-        plot(xh, yh, "ko", alpha=0.2)
+        plt.hist(data, bins=bins, histtype="stepfilled", alpha=0.3,
+            color="grey", normed=True)
+        plt.plot(xh, yh, "ko", alpha=0.2)
         
         # array for plotting
-        xx = linspace(data.min(), data.max(), 201)
+        xx = np.linspace(data.min(), data.max(), 201)
         
         # fitted double gaussian
         yy = stats.norm.pdf(xx, *p)
-        plot( xx, yy, c="r", alpha=0.8, lw=2, label="fit" )
+        plt.plot(xx, yy, c="r", alpha=0.8, lw=2, label="fit")
         
         if label:
-            xlabel(label)
-            ylabel(r"$f($" + label + r"$)$")
+            plt.xlabel(label)
+            plt.ylabel(r"$f($" + label + r"$)$")
         
-        legend(frameon=False)
+        plt.legend(frameon=False)
         
-        show()
+        plt.show()
     
     
     return p, cov
--- fmttime.py
+++ fmttime.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -----------------------------------------------------------------------------
-# FMTTIME
+# TOOLBOX.FMTTIME
 # Laura L Watkins [lauralwatkins@gmail.com]
 # -----------------------------------------------------------------------------
 
-from numpy import abs, log, pi, sqrt
 
-def fmttime( time ):
+def fmttime(time):
     
     """
     Output time elapsed in seconds into a sensible format.
@@ -15,24 +14,23 @@ def fmttime( time ):
       time : input time elapsed [seconds]
     """
     
-    
     sc = time % 60.
-    time = ( time - sc ) / 60.
-    result = "{:}s".format( sc )
+    time = (time-sc)/60.
+    result = "{:}s".format(sc)
     if not time: return result
     
-    mn = int( time % 60. )
-    time = ( time - mn ) / 60.
-    result = "{:}m {:}".format( mn, result )
+    mn = int(time % 60.)
+    time = (time-mn)/60.
+    result = "{:}m {:}".format(mn, result)
     if not time: return result
     
-    hr = int( time % 24. )
-    time = ( time - hr ) / 24.
-    result = "{:}h {:}".format( hr, result )
+    hr = int(time % 24.)
+    time = (time-hr)/24.
+    result = "{:}h {:}".format(hr, result)
     if not time: return result
     
-    dy = int( time % 7. )
-    time = ( time - dy ) / 7.
-    result = "{:}d {:}".format( dy, result )
+    dy = int(time % 7.)
+    time = (time-dy)/7.
+    result = "{:}d {:}".format(dy, result)
     
     return result
