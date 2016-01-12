Fix LIMS handling of units in errors to make it work.

lims.py
@@ -29,15 +29,19 @@ def lims(x, f=0., log=False, err=0, pivot=None):
     
     try: unit, x = x.unit, x.value
     except: unit = 1.
-    xerr = err/unit
     
     # check if plus/minus errors are different
-    if np.shape(xerr) == (2,) + np.shape(x):
-        perr = xerr[0]
-        merr = xerr[1]
+    if np.shape(err) == (2,) + np.shape(x):
+        perr = err[0]
+        merr = err[1]
     else:
-        perr = xerr
-        merr = xerr
+        perr = err
+        merr = err
+    
+    try: meunit, merr = merr.unit, merr.value
+    except: meunit = 1.
+    try: peunit, perr = perr.unit, perr.value
+    except: peunit = 1.
     
     xmin = np.nanmin(x-merr)
     xmax = np.nanmax(x+perr)